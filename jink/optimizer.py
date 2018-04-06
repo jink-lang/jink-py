518939026e9b94a6717b2bfba8e7c4584433cadf
Jacob Noah <jacobnoahbusiness@gmail.com>
2018-04-06 10:56:15 -0400

    Update optimizer.py

--- jink/optimizer.py
+++ jink/optimizer.py
@@ -7,27 +7,25 @@ BINOP_EVALS = {
   '*': lambda x, y: x * y
 }
 
-class Optimizer:
-  def optimize(self, ast):
-    self.ast = ast
-    optimized = []
+def optimize(ast):
+  optimized = []
+  if ast is None:
+    raise Exception("AST not found")
+  for expr in ast:
+    optimized.append(const_fold(expr))
+  return optimized
 
-    if ast is None:
-      raise Exception("AST not found")
-
-    for expr in ast:
-      optimized.append(self.const_fold(expr))
-    return optimized
-
-  def const_fold(self, expr):
-    if isinstance(expr, BinaryOperator):
-      left, right = self.const_fold(expr.left), self.const_fold(expr.right)
-      if isinstance(left, IntegerLiteral) and isinstance(right, IntegerLiteral):
-        return IntegerLiteral(int(BINOP_EVALS[expr.operator](left.value, right.value)))
-      elif isinstance(left, (FloatingPointLiteral, IntegerLiteral)) and isinstance(right, (FloatingPointLiteral, IntegerLiteral)):
-        return FloatingPointLiteral(BINOP_EVALS[expr.operator](left.value, right.value))
-    elif isinstance(expr, Assignment):
-      expr.value = self.const_fold(expr.value)
-    elif isinstance(expr, Function):
-      expr.body = self.const_fold(expr.body)
-    return expr
+def const_fold(expr):
+  if isinstance(expr, BinaryOperator):
+    left, right = const_fold(expr.left), const_fold(expr.right)
+    if isinstance(left, IntegerLiteral) and isinstance(right, IntegerLiteral):
+      return IntegerLiteral(int(BINOP_EVALS[expr.operator](left.value, right.value)))
+    elif isinstance(left, (FloatingPointLiteral, IntegerLiteral)) and isinstance(right, (FloatingPointLiteral, IntegerLiteral)):
+      return FloatingPointLiteral(BINOP_EVALS[expr.operator](left.value, right.value))
+  elif isinstance(expr, Assignment):
+    expr.value = const_fold(expr.value)
+  elif isinstance(expr, Function):
+    expr.body = const_fold(expr.body)
+  elif isinstance(expr, CallExpression):
+    expr.args = [const_fold(e) for e in expr.args]
+  return expr
