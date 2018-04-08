1769e2310258ed06313bb0a82daa4e9cd14b80c4
Jacob Noah <jacobnoahbusiness@gmail.com>
2018-04-08 00:00:13 -0400

    Update main.py
    Main file can now handle executing Jink files with or without extension

--- main.py
+++ main.py
@@ -1,45 +1,24 @@
 import sys
 from jink.lexer import Lexer
 from jink.parser import Parser
-from jink.validator import validate
 from jink.optimizer import optimize
 from jink.interpreter import Interpreter
 from jink.classes import Environment
-
 lexer = Lexer()
 parser = Parser()
 interpreter = Interpreter()
 
-code = """
-int add(int a, int b, int c) {
-  int result = a + b + c
-  return result
-}
+code = ''
+if len(sys.argv) > 1:
+  if not sys.argv[1].endswith('.jk'):
+    sys.argv[1] += '.jk'
+  code = open(sys.argv[1]).read()
+else:
+  raise Exception("File path not given.")
 
-int result = add(1, 2, 3)
-print(result)
-"""
+if not code:
+  raise Exception("Error in file reading.")
 
-code2 = """
-int hi() {
-  return 1
-}
-
-print(hi())
-"""
-
-code3 = """
-float my_function(int make_float) {
-  make_float = make_float + 0.0
-  float(make_float)
-}
-
-my_function(hey_earth) :: Returns 1337.0
-"""
-
-# if len(sys.argv) > 1:
-#   code = open(sys.argv[1]).read()
-
-env = Environment(None)
-AST = validate(optimize(parser.parse(lexer.parse(code))))
+env = Environment()
+AST = optimize(parser.parse(lexer.parse(code)))
 interpreter.evaluate(AST, env)
