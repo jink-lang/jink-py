1738a97ac09197c7e43e6e4df8478500f358dfa9
Jacob Noah <jacobnoahbusiness@gmail.com>
2018-04-18 01:46:06 -0400

    Scope fixes
    I removed the newlines skip in the parser and that messed some stuff up so I added it back haha
    
    Anyways, let's get into this. Recursive stuff is handled way better now, and scope actually works. I also added numbers to the environments to see how deeply nested the scopes are in testing.

--- jink.py
+++ jink.py
@@ -19,6 +19,8 @@ else:
 if not code:
   raise Exception("Error in file reading.")
 
-env = Environment()
+env = Environment(None, 0)
+env.def_func('print', lambda scope, args: print('\n'.join([str(x) for x in args]) or 'null'))
+env.def_func('string', lambda args: [str(x or 'null') for x in args][0] if len(args) == 1 else [str(x or 'null') for x in args])
 AST = optimize(parser.parse(lexer.parse(code)))
 interpreter.evaluate(AST, env)
