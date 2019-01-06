90f7ad4875261d25277245888bb72407f65c7aa4
Jacob Noah <jacobnoahbusiness@gmail.com>
2019-01-06 18:13:18 -0500

    Update jink.py
    Added an AST-printing option, preparations for verbose argument, input global function

--- jink.py
+++ jink.py
@@ -1,4 +1,5 @@
 import sys
+import json
 from jink.lexer import Lexer
 from jink.parser import Parser
 from jink.optimizer import optimize
@@ -12,19 +13,32 @@ env = Environment()
 
 env.def_func('print', lambda scope, args: print('\n'.join([str(x) for x in args]) or 'null'))
 env.def_func('string', lambda scope, args: [str(x or 'null') for x in args][0] if len(args) == 1 else [str(x or 'null') for x in args])
+env.def_func('input', lambda scope, args: input(' '.join(args)))
 
 code = ''
-if len(sys.argv) > 1:
-  if not sys.argv[1].endswith('.jk'):
-    sys.argv[1] += '.jk'
-  code = open(sys.argv[1]).read()
+if len(sys.argv) > 1 and '-v' not in sys.argv:
+  sys.argv.pop(0)
+  _path = ' '.join(sys.argv)
+  if not _path.endswith('.jk'):
+    _path += '.jk'
+  code = open(_path).read()
 
   if not code:
-    raise Exception(f"Error reading file {sys.argv[1]}")
+    raise Exception(f"Error reading file {sys.argv[0]}")
 
   AST = optimize(parser.parse(lexer.parse(code)))
+
+  # For testing (printing the AST)
+  # _AST = ""
+  # for expr in AST:
+  #   _AST += (f"{expr}, ").replace("'", '"')
+  # print(_AST[:-2])
+
   interpreter.evaluate(AST, env)
 else:
-  repl = REPL(sys.stdin, sys.stdout, env)
+  if '-v' in sys.argv:
+    repl = REPL(sys.stdin, sys.stdout, env, verbose=True)
+  else:
+    repl = REPL(sys.stdin, sys.stdout, env)
   while True:
     repl.main_loop()
