2f804db21f5de6315bbf1b4ff0d31dbcad35038b
Jekb <jacobnoahbusiness@gmail.com>
2021-09-11 06:39:38 -0400

    Fix/update unit tests

--- tests.py
+++ tests.py
@@ -1,7 +1,8 @@
 import unittest
 from jink.lexer import Lexer
 from jink.parser import Parser
-from jink.utils.future_iter import FutureIter
+from jink.utils.classes import *
+from jink.utils.func import pickle
 
 class LexerTest(unittest.TestCase):
   def setUp(self):
@@ -11,31 +12,31 @@ class LexerTest(unittest.TestCase):
     """Ensures strings are interpreted properly."""
     code = "'hi'"
     lexed = self.lexer.parse_literal(code)
-    assert lexed == "['{string hi}']", "Issue in string tokenization."
+    assert lexed == "['{TokenType.STRING hi}']", "Issue in string tokenization."
 
   def test_string_escape(self):
     """Ensures the escape character lexes properly."""
     code = "let hey_there = '\\'Hello world\\''"
     lexed = self.lexer.parse_literal(code)
-    assert lexed == "['{keyword let}', '{identifier hey_there}', '{operator =}', \"{string 'Hello world'}\"]", "Issue in escape character lexical analysis."
+    assert lexed == "['{TokenType.KEYWORD let}', '{TokenType.IDENTIFIER hey_there}', '{TokenType.OPERATOR =}', \"{TokenType.STRING 'Hello world'}\"]", "Issue in escape character lexical analysis."
 
   def test_assignment_1(self):
     """Ensures variable declaration and assignment are lexed properly."""
     code = "let hello = 'world'"
     lexed = self.lexer.parse_literal(code)
-    assert lexed == "['{keyword let}', '{identifier hello}', '{operator =}', '{string world}']", "Issue in variable declaration tokenization."
+    assert lexed == "['{TokenType.KEYWORD let}', '{TokenType.IDENTIFIER hello}', '{TokenType.OPERATOR =}', '{TokenType.STRING world}']", "Issue in variable declaration tokenization."
 
   def test_assignment_2(self):
     """Ensures variable declaration and assignment are lexed properly."""
     code = "let pi = 3.14"
     lexed = self.lexer.parse_literal(code)
-    assert lexed == "['{keyword let}', '{identifier pi}', '{operator =}', '{number 3.14}']", "Issue in variable declaration tokenization."
+    assert lexed == "['{TokenType.KEYWORD let}', '{TokenType.IDENTIFIER pi}', '{TokenType.OPERATOR =}', '{TokenType.NUMBER 3.14}']", "Issue in variable declaration tokenization."
 
   def test_call_1(self):
     """Ensures function calls are lexed properly."""
     code = "print('Hello world!')"
     lexed = self.lexer.parse_literal(code)
-    assert lexed == "['{identifier print}', '{lparen (}', '{string Hello world!}', '{rparen )}']", "Issue in function call tokenization."
+    assert lexed == "['{TokenType.IDENTIFIER print}', '{TokenType.LPAREN (}', '{TokenType.STRING Hello world!}', '{TokenType.RPAREN )}']", "Issue in function call tokenization."
 
 
 class ParserTest(unittest.TestCase):
@@ -47,29 +48,58 @@ class ParserTest(unittest.TestCase):
     """Ensures function calls are parsed properly."""
     code = "print('hello')"
     tokens = self.lexer.parse(code)
-    parsed = self.parser.parse_literal(tokens)
-    assert parsed == "[{ 'type': 'CallExpression', 'contents': { 'name': '{ 'type': 'IdentLiteral', 'contents': { 'name': 'print', 'index': 'null' } }', 'args': [{ 'type': 'StringLiteral', 'contents': { 'value': 'hello' } }] } }]", "Issue in function call parsing."
+    parsed = self.parser.parse_literal(tokens)[0]
+    test = CallExpression(
+      name=IdentLiteral(name='print'),
+      args=[StringLiteral('hello')]
+    )
+    assert pickle(parsed) == pickle(test), "Issue in function call parsing."
 
   def test_math(self):
     """Ensures arithmetic is parsed properly."""
     code = "5 + 5 / 2"
     tokens = self.lexer.parse(code)
-    parsed = self.parser.parse_literal(tokens)
-    assert parsed == "[{ 'type': 'BinaryOperator', 'contents': { 'operator': '+', 'left': '{ 'type': 'IntegerLiteral', 'contents': { 'value': 5 } }', 'right': '{ 'type': 'BinaryOperator', 'contents': { 'operator': '/', 'left': '{ 'type': 'IntegerLiteral', 'contents': { 'value': 5 } }', 'right': '{ 'type': 'IntegerLiteral', 'contents': { 'value': 2 } }' } }' } }]", "Issue in arithmetic parsing."
+    parsed = self.parser.parse_literal(tokens)[0]
+    test = BinaryOperator(operator='+',
+      left=IntegerLiteral(5),
+      right=BinaryOperator(operator='/',
+        left=IntegerLiteral(5),
+        right=IntegerLiteral(2)
+      )
+    )
+    assert pickle(parsed) == pickle(test), "Issue in arithmetic parsing."
 
   def test_assignment_1(self):
     """Ensures variable declaration and assignment are parsed properly."""
     code = "let test = 5 * 5 / 5"
     tokens = self.lexer.parse(code)
-    parsed = self.parser.parse_literal(tokens)
-    assert parsed == "[{ 'type': 'Assignment', 'contents': { 'type': 'let', 'ident': '{ 'type': 'IdentLiteral', 'contents': { 'name': 'test', 'index': 'null' } }', 'value': '{ 'type': 'BinaryOperator', 'contents': { 'operator': '/', 'left': '{ 'type': 'BinaryOperator', 'contents': { 'operator': '*', 'left': '{ 'type': 'IntegerLiteral', 'contents': { 'value': 5 } }', 'right': '{ 'type': 'IntegerLiteral', 'contents': { 'value': 5 } }' } }', 'right': '{ 'type': 'IntegerLiteral', 'contents': { 'value': 5 } }' } }' } }]", "Issue in assignment parsing."
+    parsed = self.parser.parse_literal(tokens)[0]
+    test = Assignment(_type='let',
+      ident=IdentLiteral(name='test'),
+      value=BinaryOperator(operator='/',
+        left=BinaryOperator(operator='*',
+          left=IntegerLiteral(5),
+          right=IntegerLiteral(5)
+        ),
+        right=IntegerLiteral(5)
+      )
+    )
+    assert pickle(parsed) == pickle(test), "Issue in assignment parsing."
 
   def test_conditional_1(self):
     """Ensures conditionals are parsed properly."""
     code = "if (1 == 1) return 1"
     tokens = self.lexer.parse(code)
-    parsed = self.parser.parse_literal(tokens)
-    assert parsed == "[{ 'type': 'Conditional', 'contents': { 'type': 'if', 'expression': { 'type': 'BinaryOperator', 'contents': { 'operator': '==', 'left': '{ 'type': 'IntegerLiteral', 'contents': { 'value': 1 } }', 'right': '{ 'type': 'IntegerLiteral', 'contents': { 'value': 1 } }' } }, 'body': [{ 'type': 'Return', contents: { 'expression': { 'type': 'IntegerLiteral', 'contents': { 'value': 1 } } } }], 'else_body': [] } }]", "Issue in inline conditional parsing."
+    parsed = self.parser.parse_literal(tokens)[0]
+    test = Conditional('if',
+      expression=BinaryOperator(operator='==',
+        left=IntegerLiteral(1),
+        right=IntegerLiteral(1)
+      ),
+      body=[Return(IntegerLiteral(1))],
+      else_body=[]
+    )
+    assert pickle(parsed) == pickle(test), "Issue in inline conditional parsing."
 
 
 if __name__ == "__main__":
